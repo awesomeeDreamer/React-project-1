17b008ee495eccff7c1731b1288c8aaa52c8fa01
awesomeeDreamer <160412077+awesomeeDreamer@users.noreply.github.com>
2024-03-22 18:43:40 +0530

    Add files via upload

---
+++ Hero.jsx
@@ -0,0 +1,22 @@
+const Hero = () => {
+  return (
+    <main className="hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
+          deserunt, corporis, officia quas voluptate itaque sed ducimus optio
+          fuga esse distinctio ipsa eum vero, a consequuntur laudantium illo
+          placeat? Veritatis.
+        </p>
+        <div className="hero-btn">
+          <button className="shp">Shop Now</button>
+          <button className="cte">Category</button>
+        </div>
+       
+      </div>
+      <div className="hero-img"></div>
+    </main>
+  );
+};
+export default Hero;
